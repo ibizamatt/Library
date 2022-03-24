03b95abfb5fe29dc64d1c9447897d1984d7335d0
Matt McEvoy <ibizamatt@yahoo.co.uk>
2022-03-24 18:18:08 +1300

    commit

---
+++ src/files/PayLoads.java
@@ -0,0 +1,15 @@
+package files;
+
+public class PayLoads {
+
+	public static String AddPlace()
+	{
+		return "{\r\n" + 
+				"\"name\":\"Zafraan's New Book\",\r\n" + 
+				"\"isbn\":\"Z9\",\r\n" + 
+				"\"aisle\":\"123\",\r\n" + 
+				"\"author\":\"Matt McEvoy\"\r\n" + 
+				"}";
+	}
+
+}
